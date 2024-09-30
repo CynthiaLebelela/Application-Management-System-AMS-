780770919a87ee55a7f76cebff38ccbcfccde819
Cynthia <135084120+CynthiaLebelela@users.noreply.github.com>
2024-09-30 19:01:06 +0200

    Update track_application.php
    did the css for the track application file

--- track_application.php
+++ track_application.php
@@ -38,12 +38,51 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_num'])) {
 <head>
     <meta charset="UTF-8">
     <title>Track Application</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="style(1).css" rel="stylesheet" />
+    <style>
+        h1 {
+            text-align: center;
+            margin-top: 20px;
+            margin-bottom: 20px;
+            color: #ECF0F1;
+        }
+
+        .mb-3 input {
+            width: 300px;
+            margin-left: 650px;
+            margin-top: 140px;
+        }
+
+        button {
+            background-color: #369481;
+            color: #ECF0F1;
+            font-weight: medium;
+            margin-left: 850px;
+            border: none;
+            border-radius: 5px;
+            padding: 5px;
+        }
+
+        button:hover {
+            background-color: #1ABC9C;
+        }
+
+        img {
+            width: 300px;
+            margin-left: 20px;
+        }
+    </style>
 </head>
 
 <body>
     <h1>Track Your Application</h1>
     <form method="POST">
-        <input type="text" name="id_num" placeholder="Enter your ID number" required>
+        <img src="user.svg" alt="track-application icon" style="color: #ECF0F1;">
+        <div class="mb-3">
+            <input type="text" name="id_num" class="form-control" id="id_num" placeholder="Enter your ID number"
+                required>
+        </div>
         <button type="submit">Check Status</button>
     </form>
     <p><?= $status_message ?></p>
